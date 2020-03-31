8c739b3274a3014b7750be85b5651e033dbd6c46
Glitch (outstanding-bubble-planet) <none>
2020-03-31 12:21:52 +0000

    👦🐡 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,52 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+// request for new timestamp
+app.get("/api/timestamp/", function(req, res){
+        res.json({unix: Date.now(), utc: Date()})
+});
+
+//request to convert timestamp and return JSON
+app.get("/api/timestamp/:date_string?", function (req, res) {
+      let d_string = req.params.date_string
+      
+      if (/\d{5,}/.test(d_string)) {
+        let d_int = parseInt(d_string);
+        
+        res.json({unix: d_string, UTC: new Date(d_int).toUTCString()});
+      }
+  
+  let date_obj = new Date(d_string);
+  if (date_obj.toString() == "Invalid Date"){
+     res.json({"error" : "Invalid Date" });
+  } else {
+    res.json({ unix: date_obj.valueOf(), utc: date_obj.toUTCString() });
+  }
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
